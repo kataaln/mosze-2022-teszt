e88b34baac8f21872f28a5ada3c3d40276c46cea
kataaln <szkataalin@gmail.com>
2022-09-08 19:40:20 +0200

    hibák javítva

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 constexpr int N_ELEMENTS = 100;
 
 int main() {
-  int* b = new int[NELEMENTS];           // eliras
-  std::cout << '1-100 ertekek duplazasa' // "", lezaras
-      for (int i = 0;)                   // for ciklus
+  int* b = new int[N_ELEMENTS];           // eliras
+  std::cout << "1-100 ertekek duplazasa"; // "", lezaras
+  for (int i = 1; i <= N_ELEMENTS; i++)   // for ciklus
   {
     b[i] = i * 2;
   }
-  for (int i = 0; i; i++) // for ciklus
+  for (int i = 1; i <= N_ELEMENTS; i++) // for ciklus
   {
-    std::cout << "Ertek:" // ertek nem irja, lezaras ;
+    std::cout << "Ertek:" << b[i] << std::endl; // ertek nem irja, lezaras ;
   }
   std::cout << "Atlag szamitasa: " << std::endl;
-  int atlag;                           // int helyett double, atlag = 0
-  for (int i = 0; i < N_ELEMENTS, i++) // for ciklus
+  double atlag = 0;                     // int helyett double, atlag = 0
+  for (int i = 1; i <= N_ELEMENTS; i++) // for ciklus
   {
-    atlag += b[i] // lezaras ;
+    atlag += b[i]; // lezaras ;
   }
   atlag /= N_ELEMENTS;
   std::cout << "Atlag: " << atlag << std::endl;
