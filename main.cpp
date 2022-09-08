eeecec6ed9e1c5af176854ec62f8171066ffa7f5
kataaln <szkataalin@gmail.com>
2022-09-08 19:35:46 +0200

    sok hiba

--- main.cpp
+++ main.cpp
@@ -2,25 +2,24 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+int main() {
+  int* b = new int[NELEMENTS];           // eliras
+  std::cout << '1-100 ertekek duplazasa' // "", lezaras
+      for (int i = 0;)                   // for ciklus
+  {
+    b[i] = i * 2;
+  }
+  for (int i = 0; i; i++) // for ciklus
+  {
+    std::cout << "Ertek:" // ertek nem irja, lezaras ;
+  }
+  std::cout << "Atlag szamitasa: " << std::endl;
+  int atlag;                           // int helyett double, atlag = 0
+  for (int i = 0; i < N_ELEMENTS, i++) // for ciklus
+  {
+    atlag += b[i] // lezaras ;
+  }
+  atlag /= N_ELEMENTS;
+  std::cout << "Atlag: " << atlag << std::endl;
+  return 0;
 }
